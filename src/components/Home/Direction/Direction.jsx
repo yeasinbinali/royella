eed48d35207a6151c5e02b8f3228d40dc6e7175e
yeasinbinali <yeasinbinali@gmail.com>
2024-05-10 00:10:36 +0600

    tooltip added

--- src/components/Home/Direction/Direction.jsx
+++ src/components/Home/Direction/Direction.jsx
@@ -1,13 +1,17 @@
 import { Map, Marker } from 'pigeon-maps';
 import React from 'react';
 import { osm } from 'pigeon-maps/providers'
+import { Tooltip } from 'react-tooltip';
 
 const Direction = () => {
     return (
         <div className='mb-20'>
             <h2 className='text-6xl text-center font-bold mb-10'>Our_Direction</h2>
             <Map provider={osm} height={400} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-                <Marker width={50} anchor={[50.879, 4.6997]} />
+                <Marker className='my-anchor-element' width={50} anchor={[50.879, 4.6997]} />
+                <Tooltip anchorSelect=".my-anchor-element" place="top">
+                    Royello | Luxury Hotel
+                </Tooltip>
             </Map>
         </div>
     );
