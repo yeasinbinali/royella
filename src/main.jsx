9e5485e11c0d21693c37d81b1b17300a20be472f
yeasinbinali <yeasinbinali@gmail.com>
2024-05-09 21:47:09 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from './components/Home/Home/Home.jsx';
+import Register from './components/Authentication/Register/Register.jsx';
+import Login from './components/Authentication/Login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
