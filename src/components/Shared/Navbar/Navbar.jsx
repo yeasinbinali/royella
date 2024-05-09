1514b142c84c06ed74ffa84c145e75e5ac21c3c2
yeasinbinali <yeasinbinali@gmail.com>
2024-05-09 22:16:03 +0600

    navbar active link bg added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from '../../../../images/royella_logo.png'
+import logo from '../../../../images/royella_logo.png';
 import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     const lists = [
-        <li><NavLink className='text-complex' to='/'>Home</NavLink></li>,
-        <li><NavLink className='text-complex' to='/rooms'>Rooms</NavLink></li>,
-        <li><NavLink className='text-complex' to='/about'>About</NavLink></li>,
-        <li><NavLink className='text-complex' to='/contact'>Contact</NavLink></li>
+        <li><NavLink className='text-complex text-xl' to='/'>Home</NavLink></li>,
+        <li><NavLink className='text-complex text-xl' to='/rooms'>Rooms</NavLink></li>,
+        <li><NavLink className='text-complex text-xl' to='/about'>About</NavLink></li>,
+        <li><NavLink className='text-complex text-xl' to='/contact'>Contact</NavLink></li>
     ]
     return (
         <div className="navbar bg-simple py-6">
@@ -16,14 +17,14 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul id='nav-item' tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {lists}
                     </ul>
                 </div>
                 <img src={logo} alt="logo" />
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <ul id='nav-item' className="menu menu-horizontal px-1">
                     {lists}
                 </ul>
             </div>
