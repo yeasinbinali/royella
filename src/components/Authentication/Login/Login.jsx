28ada5ef9c3db15b598a072fea5420564658ec16
yeasinbinali <yeasinbinali@gmail.com>
2024-05-10 21:43:52 +0600

    register page added

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useForm } from "react-hook-form"
 import { Link } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
+import { Tooltip } from 'react-tooltip';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => console.log(data)
     return (
         <div className='mt-10 mb-20 w-1/3 p-10 mx-auto bg-[whitesmoke]'>
+            <div className='p-10 bg-simple w-[120px] h-[120px] rounded-full mx-auto text-center'><img src="https://i.ibb.co/KwZnwMt/section-shape1.png" alt="" /></div>
             <h1 className='text-5xl title-font text-center my-8'>Login</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form className='mb-5' onSubmit={handleSubmit(onSubmit)}>
                 <div className='w-full mx-auto mb-5'>
                     <label>Email</label><br />
-                    <input placeholder='Your Email' className='border-[1px] w-full p-1' {...register("email")} />
+                    <input placeholder='Your Email' type='email' className='border-[1px] w-full p-1' {...register("email")} />
                 </div>
                 <div className='w-full mx-auto mb-6'>
                     <label>Password</label><br />
-                    <input placeholder='Your Password' className='border-[1px] w-full p-1' {...register("password")} />
+                    <input placeholder='Your Password' type='password' className='border-[1px] w-full p-1' {...register("password")} />
                 </div>
                 <input className='btn bg-main text-complex w-full btn-sm hover:bg-simple' type="submit" />
             </form>
-            <p className='my-5 text-center'>New to Royella ? <Link to='/register'>Register</Link></p>
+            <div className="divider">OR</div>
+            <div className='flex justify-center mt-5'>
+                <FcGoogle className='text-3xl my-anchor-element' />
+                <Tooltip anchorSelect=".my-anchor-element" place="top">
+                    Google Sign-in
+                </Tooltip>
+            </div>
+            <p className='my-5 text-center'>New to Royella ? <span className='font-bold'><Link to='/register'>Register</Link></span></p>
         </div>
     );
 };
--- src/components/Authentication/Register/Register.jsx
+++ src/components/Authentication/Register/Register.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useForm } from "react-hook-form"
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data)
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='mt-10 mb-20 w-1/3 p-10 mx-auto bg-[whitesmoke]'>
+            <div className='p-10 bg-simple w-[120px] h-[120px] rounded-full mx-auto text-center'><img src="https://i.ibb.co/KwZnwMt/section-shape1.png" alt="" /></div>
+            <h1 className='text-5xl title-font text-center my-8'>Register</h1>
+            <form className='mb-5' onSubmit={handleSubmit(onSubmit)}>
+                <div className='w-full mx-auto mb-5'>
+                    <label>Full Name</label><br />
+                    <input placeholder='Your Name' type='text' className='border-[1px] w-full p-1' {...register("name")} />
+                </div>
+                <div className='w-full mx-auto mb-5'>
+                    <label>Photo URL</label><br />
+                    <input placeholder='Your Photo' type='text' className='border-[1px] w-full p-1' {...register("photo")} />
+                </div>
+                <div className='w-full mx-auto mb-5'>
+                    <label>Email</label><br />
+                    <input placeholder='Your Email' type='email' className='border-[1px] w-full p-1' {...register("email")} />
+                </div>
+                <div className='w-full mx-auto mb-6'>
+                    <label>Password</label><br />
+                    <input placeholder='Your Password' type='password' className='border-[1px] w-full p-1' {...register("password")} />
+                </div>
+                <input className='btn bg-main text-complex w-full btn-sm hover:bg-simple' type="submit" />
+            </form>
+            <p className='my-5 text-center'>Already have an account ? <span className='font-bold'><Link to='/login'>Login</Link></span></p>
         </div>
     );
 };
