890ad98d4de88a862ccb393b66ef849f6a786579
yeasinbinali <yeasinbinali@gmail.com>
2024-05-14 20:12:35 +0600

    newsletter section added

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Direction from '../Direction/Direction';
 import FeaturedRooms from '../FeaturedRooms/FeaturedRooms';
+import NewsLetter from '../NewsLetter/NewsLetter';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Direction></Direction>
             <FeaturedRooms></FeaturedRooms>
+            <NewsLetter></NewsLetter>
         </div>
     );
 };
---
+++ src/components/Home/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import newsletter from '../../../../images/subscribe.webp';
+
+const NewsLetter = () => {
+    return (
+        <div className='mb-20 w-[90%] mx-auto flex justify-between items-center gap-10'>
+            <div className='w-[35%] mx-auto'>
+                <img src={newsletter} className='rounded-full' alt="" />
+            </div>
+            <div className='w-[50%] mx-auto'>
+                <h1 className='text-5xl font-bold'>Subscribe to our newsletter</h1>
+                <p className='my-5'>Stay Ahead of the Curve with Royella's Exclusive Newsletter! Looking for the hottest hotel deals, exclusive offers, and insider updates? Look no further! Subscribe to Royella's newsletter today and unlock a world of travel possibilities!</p>
+                <input type="email" className='border-[3px] w-[75%] p-2 border-[gray]' placeholder='Your E-mail' />
+                <input type="submit" className='btn bg-main text-complex btn-md rounded-none' />
+            </div>
+        </div>
+    );
+};
+
+export default NewsLetter;
