d9b0e77016f512592994eecf370a24043d8e8f62
yeasinbinali <yeasinbinali@gmail.com>
2024-05-15 22:10:39 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Shared/Navbar/Navbar'
+import Footer from './components/Shared/Footer/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   )
 }
